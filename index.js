94da9a64b2effed4a5d5ca1450bb4fad7c802fb0
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-01 14:21:55 +0530

    Factorial of a number

--- index.js
+++ index.js
@@ -9,10 +9,29 @@ function fibonacci(n) {
   }
   return feb;
 }
-
+console.log("------------------------------");
 console.log("Fibonacci Sequence");
+console.log("------------------------------");
 console.log(fibonacci(2)); // [0,1]
 console.log(fibonacci(5)); // [0,1,1,2,3]
 console.log(fibonacci(7)); // [0,1,1,2,3,5,8]
+// Big-O = O(n)
+
+///////////////////////////////
+//// Factorial of a number ////
+///////////////////////////////
+
+function factorial(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
+  }
+  return result;
+}
 console.log("------------------------------");
-// Big-O ; O(n)
+console.log("Factorial of a Number");
+console.log("------------------------------");
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(5)); // 120
+// Big-O = O(n)
