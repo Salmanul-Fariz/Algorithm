bc8a8832d76d4b000e7548e33b40bff72ded214f
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-05 21:43:46 +0530

    Recursive Factorial Number

--- Math-Algorithm.js
+++ Math-Algorithm.js
@@ -9,9 +9,9 @@ function fibonacci(n) {
   }
   return feb;
 }
-console.log("------------------------------");
-console.log("Fibonacci Sequence");
-console.log("------------------------------");
+console.log('------------------------------');
+console.log('Fibonacci Sequence');
+console.log('------------------------------');
 console.log(fibonacci(2)); // [0,1]
 console.log(fibonacci(5)); // [0,1,1,2,3]
 console.log(fibonacci(7)); // [0,1,1,2,3,5,8]
@@ -28,9 +28,9 @@ function factorial(n) {
   }
   return result;
 }
-console.log("------------------------------");
-console.log("Factorial of a Number");
-console.log("------------------------------");
+console.log('------------------------------');
+console.log('Factorial of a Number');
+console.log('------------------------------');
 console.log(factorial(0)); // 1
 console.log(factorial(1)); // 1
 console.log(factorial(5)); // 120
@@ -54,9 +54,9 @@ function isPrime(n) {
   return true;
 }
 
-console.log("------------------------------");
-console.log("Prime or Non-Prime");
-console.log("------------------------------");
+console.log('------------------------------');
+console.log('Prime or Non-Prime');
+console.log('------------------------------');
 console.log(isPrime(1)); // false
 console.log(isPrime(3)); // true
 console.log(isPrime(5)); // true
@@ -75,9 +75,9 @@ function isPowerOfTwo(n) {
   return (n & (n - 1)) === 0;
 }
 
-console.log("------------------------------");
-console.log("Power of Two");
-console.log("------------------------------");
+console.log('------------------------------');
+console.log('Power of Two');
+console.log('------------------------------');
 console.log(isPowerOfTwo(1)); // true
 console.log(isPowerOfTwo(2)); // true
 console.log(isPowerOfTwo(5)); // false
@@ -92,10 +92,27 @@ function recursiveFbonacci(n) {
 
   return recursiveFbonacci(n - 1) + recursiveFbonacci(n - 2);
 }
-console.log("------------------------------");
-console.log("Recursive Fibonacci Sequence");
-console.log("------------------------------");
+console.log('------------------------------');
+console.log('Recursive Fibonacci Sequence');
+console.log('------------------------------');
 console.log(recursiveFbonacci(0)); // 0
 console.log(recursiveFbonacci(1)); // 1
-console.log(recursiveFbonacci(3)); // 8
+console.log(recursiveFbonacci(6)); // 8
 // Big-O = O(n^2) - recursive fibonacci is not good
+
+////////////////////////////////////
+//// Recursive Factorial Number ////
+////////////////////////////////////
+
+function recursiveFactorial(n) {
+  if (n === 0) return 1;
+
+  return n * recursiveFactorial(n - 1);
+}
+console.log('------------------------------');
+console.log('Recursive Factorial Number');
+console.log('------------------------------');
+console.log(recursiveFactorial(0)); // 1
+console.log(recursiveFactorial(1)); // 1
+console.log(recursiveFactorial(5)); // 120
+// Big-O = O(n)
