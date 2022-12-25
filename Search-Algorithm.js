b49bdf68688f77de7f8245552188e6189d9eaafb
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-25 16:01:09 +0530

    Recursive Binary Search

--- Search-Algorithm.js
+++ Search-Algorithm.js
@@ -46,3 +46,34 @@ console.log(binarySearch([-5, 2, 4, 6, 10], 10)); // 4
 console.log(binarySearch([-5, 2, 4, 6, 10], 6)); // 3
 console.log(binarySearch([-5, 2, 4, 6, 10], 20)); // -1
 // Big-O = O(logn)
+
+///////////////////////////////
+/// Recursive Binary Search ///
+///////////////////////////////
+
+// Note : In this i use Immediate Functions in JavaScript
+function recursiveBinarySearch(arr, target) {
+  return (function Search(arr, target, leftIndex, rightIndex) {
+    if (leftIndex > rightIndex) {
+      return -1;
+    }
+
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (target === arr[middleIndex]) {
+      return middleIndex;
+    }
+
+    if (target < arr[middleIndex]) {
+      return Search(arr, target, 0, middleIndex - 1);
+    } else {
+      return Search(arr, target, middleIndex + 1, rightIndex);
+    }
+  })(arr, target, 0, arr.length - 1);
+}
+console.log('------------------------------');
+console.log('Recursive Binary Search');
+console.log('------------------------------');
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 10)); // 4
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 6)); // 3
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 20)); // -1
+// Big-O = O(logn)
