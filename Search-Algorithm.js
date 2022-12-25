f27447d6a7c2c80be65a542d88d9b79ce49c301b
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-25 15:36:52 +0530

    Binary Search

--- Search-Algorithm.js
+++ Search-Algorithm.js
@@ -15,3 +15,34 @@ console.log(linearSearch([-5, 2, 10, 4, 6], 10)); // 2
 console.log(linearSearch([-5, 2, 10, 4, 6], 6)); // 4
 console.log(linearSearch([-5, 2, 10, 4, 6], 20)); // -1
 // Big-O = O(n)
+
+///////////////////////////////
+//////// Binary Search ////////
+///////////////////////////////
+
+function binarySearch(arr, target) {
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (target === arr[middleIndex]) {
+      return middleIndex;
+    }
+
+    if (target < arr[middleIndex]) {
+      rightIndex = middleIndex - 1;
+    } else {
+      leftIndex = middleIndex + 1;
+    }
+  }
+
+  return -1;
+}
+console.log('------------------------------');
+console.log('Binary Search');
+console.log('------------------------------');
+console.log(binarySearch([-5, 2, 4, 6, 10], 10)); // 4
+console.log(binarySearch([-5, 2, 4, 6, 10], 6)); // 3
+console.log(binarySearch([-5, 2, 4, 6, 10], 20)); // -1
+// Big-O = O(n)
