28426caea39705d1dd2e4f7541d9a2b49a0b74e3
Salmanul Fariz N <salmanulfariz997@gmail.com>
2023-08-14 16:49:08 +0400

    Creted the bubble sorting function

--- sort/bubble_sort.js
+++ sort/bubble_sort.js
@@ -1,11 +1,17 @@
-function selectionSorting(array) {
-  for (let i = 0; i < array.length - 1; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[j] < array[i]) {
-        swapValues(array, i, j);
+function bubbleSorting(array) {
+  let swapped;
+
+  do {
+    swapped = false;
+
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        swapValues(array, i, i + 1);
+
+        swapped = true;
       }
     }
-  }
+  } while (swapped);
 }
 
 // For Swap the Values
@@ -16,11 +22,11 @@ function swapValues(array, value1Index, value2Index) {
 }
 
 console.log('------------------------------');
-console.log('Selection Sorting');
+console.log('Bubble Sorting');
 console.log('------------------------------');
 
 const array = [8, 20, -2, 4, -6];
-selectionSorting(array);
+bubbleSorting(array);
 
 console.log(array);
 // Output :- [-6, -2, 4, 8, 20]
